9422507e113deb6fbc04ca844c8bce90c27eab7b
Sungmin Oh <saogig0503@naver.com>
2022-02-15 12:15:41 +0900

    first commit

---
+++ scripts/excelprac.py
@@ -0,0 +1,80 @@
+import openpyxl
+
+sourceFileAddress = r'C:\Users\saogi\Desktop\files\2021\사료 검정기록서(21.10).xlsx'
+revisedFileAddress = r'C:\Users\saogi\Desktop\files\2021 검정대장\검정대장(21.10).xlsx'
+
+wb1 = openpyxl.load_workbook(sourceFileAddress)
+wb2 = openpyxl.load_workbook(revisedFileAddress)
+
+ws1 = wb1['Sheet1']
+ws2 = wb2['Sheet1']
+
+validrows = []
+examineitemnumbers = []
+
+numToIngredient = {1:'A', 2:'B', 3:'수분', 4:'조회분', 5:'조단백', 6:'조지방', 7:'조섬유', 8:'NDF', 9:'ADF', 10:'펩신소화율', 11:'염분', 12:'불소', 13:'칼슘', 14:'인', 15:'납', 16:'크롬', 17:'비소', 18:'수은', 19:'카드뮴', 20:'주석'}
+numToCol = {1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H', 9:'I', 10:'J', 11:'K', 12:'L', 13:'M', 14:'N', 15:'O', 16:'P', 17:'Q', 18:'R', 19:'S', 20:'T'}
+
+page=1
+while(page <= 125):
+  col = ws1['A']
+  for cell in col:
+    if(cell.row >= 10+(29*(page-1)) and cell.row <= 28+(29*(page-1))):
+      if(cell.value != None):
+        validrows.append(cell)
+  page = page + 1
+
+for validrow in validrows:
+  row = ws1[validrow.row]
+  examineitemnumber = -2
+  for cell in row:
+    if(cell.value != None):
+      examineitemnumber = examineitemnumber + 1
+  examineitemnumbers.append(examineitemnumber)
+
+index = 0
+page = 1
+remainingLines = 29
+for item in validrows:
+  requiredLines = examineitemnumbers[index]
+  #어떤 항목 검사했는지 알 수 있는 부분
+  itemrow = ws1[item.row]
+  times = 0
+  examinedItems = []
+  for cell in itemrow:
+    times = times + 1
+    if (times <= 2):
+      continue
+    else:
+      if(cell.value != None):
+        examinedItems.append(cell.column)
+
+  if (requiredLines > remainingLines):
+    page = page + 1
+    remainingLines = 29
+  ws2[f'A{7+(29-remainingLines)+(36 * (page-1))}'] = item.value
+  #성분명, 검정결과 삽입하는 부분
+  index2 = 0
+  for plz in examinedItems:
+    if index2 == 0:
+      ws2[f'C{7+(29-remainingLines)+(36 * (page - 1)) + index2}'] = ws1[f'B{item.row}'].value
+    ws2[f'D{7+(29-remainingLines)+(36 * (page - 1)) + index2}'] = numToIngredient[plz]
+    ws2[f'H{7+(29-remainingLines)+(36 * (page - 1)) + index2}'] = numToIngredient[plz]
+    ws2[f'I{7+(29-remainingLines)+(36 * (page - 1)) + index2}'] = ws1[f'{numToCol[plz]}{item.row}'].value
+    if (numToIngredient[plz] == '수분' or numToIngredient[plz] == '조회분'):
+      ws2[f'J{7+(29-remainingLines)+(36 * (page - 1)) + index2}'] = '정주리'
+    elif (numToIngredient[plz] == '조단백' or numToIngredient[plz] == '조지방' or numToIngredient[plz] == '조섬유' or numToIngredient[plz] == 'NDF' or numToIngredient[plz] == 'ADF' or numToIngredient[plz] == '펩신소화율' or numToIngredient[plz] == '염분' or numToIngredient[plz] == '불소'):
+      ws2[f'J{7+(29-remainingLines)+(36 * (page - 1)) + index2}'] = '김진희'
+    elif (numToIngredient[plz] == '칼슘' or numToIngredient[plz] == '인' or numToIngredient[plz] == '납' or numToIngredient[plz] == '크롬' or numToIngredient[plz] == '비소' or numToIngredient[plz] == '수은' or numToIngredient[plz] == '카드뮴' or numToIngredient[plz] == '주석'):
+      ws2[f'J{7+(29-remainingLines)+(36 * (page - 1)) + index2}'] = '김민창'
+    index2 = index2 + 1
+  remainingLines = remainingLines - requiredLines
+  if (requiredLines > remainingLines):
+    page = page + 1
+    remainingLines = 29
+  index = index + 1
+
+
+wb2.save(revisedFileAddress)
+
+#print(number)
